06b26562cf5d374368661362fe02c5fa29d9cdb3
sangtern <sang.t.nguyen@proton.me>
2025-05-09 02:22:07 -0500

    Updates and fixes to backend & frontend involving production * Backend     * Added `build` script to `package.json` to install all node       packages in backend and frontend before building the frontend     * Added account type view permissions by adding `:role` parameter to       the API GET requests     * Fixed backend server crashing due to `App.get("*")` handling         * This is probably due to conflicts involving Express and           React Router * Frontend     * Removed unused declared variables and functions since their       warnings and errors are preventing production building     * Set certain attributes of objects to have default values rather       than being null or undefined initially (for the same reasons as       above bullet point)     * Updated pages and handling of pages         * Pages now have an attribute containing a list of allowed           "roles" (account types) able to access the data table         * Note that I have not updated the pages' associated `key_index`           yet

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Register from './components/Register.tsx';
 import Dashboard from './components/Dashboard.tsx';
